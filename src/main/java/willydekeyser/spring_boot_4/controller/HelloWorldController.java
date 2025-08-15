da70b5c0fabf87a268eaf3b9871b4a2975380be1
willydekeyser <ma074172@skynet.be>
2025-08-15 17:33:03 +0200

    Create Hello World Controller

---
+++ src/main/java/willydekeyser/spring_boot_4/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package willydekeyser.spring_boot_4.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
